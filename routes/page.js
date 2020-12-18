e225720486f2563478f718f9a8b2760258a6754c
hwakyung99 <58030720+hwakyung99@users.noreply.github.com>
2020-12-18 16:45:22 +0900

    Update page.js

--- routes/page.js
+++ routes/page.js
@@ -5,14 +5,14 @@ const { Post, User, Hashtag } = require('../models');
 const router = express.Router();
 
 router.use((req, res, next) => {
-    res.locals.user = req.user;
-    res.locals.followerCount = req.user ? req.user.Followers.length : 0;
-    res.locals.followingCount = req.user ? req.user.Followings.length : 0;
-    res.locals.followerIdList = req.user ? req.user.Followings.map(f => f.id) : [];
-    next();
+  res.locals.user = req.user;
+  res.locals.followerCount = req.user ? req.user.Followers.length : 0;
+  res.locals.followingCount = req.user ? req.user.Followings.length : 0;
+  res.locals.followerIdList = req.user ? req.user.Followings.map(f => f.id) : [];
+  next();
 });
 
-router.get('/profile', isLoggedIn, async(req, res) => {
+router.get('/profile', isLoggedIn, async(req, res, next) => {
   try{
     const myposts = await Post.findAll({
       include: {
@@ -32,26 +32,38 @@ router.get('/profile', isLoggedIn, async(req, res) => {
     console.error(err);
     next(err);
   }
-  });
-  
-  router.get('/join', isNotLoggedIn, (req, res) => {
-    res.render('join', { title: '회원가입 - NodeBird'});
-  });  
-  
-  router.get('/', async(req, res, next) => {
-    try{
+});
+
+router.get('/join', isNotLoggedIn, (req, res) => {
+  res.render('join', { title: '회원가입 - NodeBird' });
+});
+
+router.delete('/:id/remove', isLoggedIn, async(req, res, next) => {
+  try{
+    const mypost = await Post.destroy({
+      where: { id: req.params.id }
+    });
+    res.json(mypost);
+  } catch (err){
+    console.error(err);
+    next(err);
+  }
+});
+
+router.get('/', async (req, res, next) => {
+  try {
     const posts = await Post.findAll({
       include: {
         model: User,
         attributes: ['id', 'nick'],
       },
-      order: [['createdAt', 'DESC']]
+      order: [['createdAt', 'DESC']],
     });
     res.render('main', {
       title: 'NodeBird',
       twits: posts,
     });
-  } catch (err){
+  } catch (err) {
     console.error(err);
     next(err);
   }
@@ -61,22 +73,22 @@ router.get('/hashtag', async (req, res, next) => {
   const query = req.query.hashtag;
   if (!query) {
     return res.redirect('/');
-  }  
-  try { 
+  }
+  try {
     const hashtag = await Hashtag.findOne({ where: { title: query } });
     let posts = [];
-    if (hashtag){
+    if (hashtag) {
       posts = await hashtag.getPosts({ include: [{ model: User }] });
-    }  
+    }
 
     return res.render('main', {
       title: `${query} | NodeBird`,
       twits: posts,
-    });  
-  } catch (error){
+    });
+  } catch (error) {
     console.error(error);
     return next(error);
-  }  
-});  
+  }
+});
 
 module.exports = router;
